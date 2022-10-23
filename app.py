6526193d5b9537aae9c0e9626925d653ea3dff3d
Muthaqin <54973773+qiqinn@users.noreply.github.com>
2022-10-23 23:06:02 +0700

    update

--- app.py
+++ app.py
@@ -1,48 +1,73 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, Request, render_template
+from werkzeug.utils import secure_filename
+import mtcd
 from PIL import Image
-from pathlib import Path
-import numpy as np
-from datetime import datetime
-from flask import Flask, request, render_template
 
-from feature_extractor import FeatureExtractor
+datahasil = os.listdir('static/result/')
+
+UPLOAD_FOLDER = 'static/uploads/'
+RESULT_FOLDER = 'static/result/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['RESULT_FOLDER'] = RESULT_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 
-# Read img features
-fe = FeatureExtractor()
-features = []
-img_paths = []
+@app.after_request
+def add_header(r):
+    
+    #Add headers to both force latest IE rendering engine or Chrome Frame,
+    #and also to cache the rendered page for 10 minutes.
+    
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
-# load features from .npy files and read img_paths
-for feature_path in Path("./static/feature").glob("*.npy"):
-    features.append(np.load(feature_path))
-    img_paths.append(Path("./static/img") / (feature_path.stem + ".jpg"))
-features = np.array(features)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        file = request.files["query_img"]
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename("queryImg.jpg")
+            data = os.path.join('static/uploads/', 'queryImg.jpg')
+            file.save(data)
+            mtcd.process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('result_file'))
+    return render_template('index.html')
 
-        # Save query img
-        img = Image.open(file.stream)  # PIL image
-        uploaded_img_path = "static/uploaded/" + \
-                            datetime.now().isoformat().replace(":", ".") + "_" + file.filename  # create img path
-        img.save(uploaded_img_path)  # save the image to the path
+@app.route('/', methods=['GET', 'POST'])
+def result_file():
+    return render_template('index.html')
 
-        # RUn search
-        fe = FeatureExtractor()
-        query = fe.extract(img=img)  # feature extract of uploaded img
-        dists = np.linalg.norm(features - query, axis=1)  # calc L2 distance
-        ids = np.argsort(dists)[0 : 9]  # sort dists return ids
-        scores = [(dists[id], img_paths[id]) for id in ids]
-        # return dists ad img path
-        # print(scores)
+@app.route('/predict', methods=['GET', 'POST'])
+def mtcd_predict():
+    if request.method == 'POST':
+        filename = request.form.get('input_image')
+        if filename and allowed_file(filename):  
+            img = Image.open(filename)
+            # img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
+            img = img.save("static/uploads/queryImg.jpg")             
+            data = os.path.join('static/uploads/queryImg.jpg')
+            mtcd.process_file(data)
+            return redirect('/')
+    return render_template('/index.html', len = len(datahasil), datahasil = datahasil)
 
-        return render_template("index.html", query_path=uploaded_img_path, scores=scores)
-    else:
-        return render_template("index.html")
-
-
-if  __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
--- static/js/main.js
+++ static/js/main.js
@@ -52,8 +52,6 @@
         $(thisAlert).removeClass('alert-validate');
     }
     
-    http.listen(5000, function () {
-        console.log('listening on *:5000');
-    });
+    
 
 })(jQuery);
